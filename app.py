09c4e3a3d74736121e28d097a1c94c3377fe37e2
Tejas Deshpande <Tejas.Deshpande@Triconinfotech.com>
2026-02-20 10:54:43 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("Hello GitHub")
+
+arr = [4,3,2,5,1]
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+print(arr)
